09b5aa90d6c6e01800e44743446d1b5821e3b254
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-22 19:04:49 +0200

    push

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" :src="imgUrl" />
+    Ciao!
+    <p class="red">{{ saluto }}</p>
+    <div v-for="(item, index) in test" :key="index">
+      {{ item }}
+      <FirstComponent />
+    </div>
+  </div>
+</template>
+
+<script>
+
+import FirstComponent from './components/first-component.vue'
+
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      saluto: 'hello world',
+      test: [1, 4, 8, 12, 1, 6],
+      imgUrl: './assets/logo.png'
+    };
+  },
+  components:{
+    FirstComponent
+  }
+};
+</script>
+
+<style lang="scss">
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/first-component.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="red" @click="saluta">
+      Il mio primo componente
+      <div class="inner">Sono Inner</div>
+    </div>
+    <input class="red active" type="text" v-model="text" />
+    <div class="inner">Inner fuori da red</div>
+    <div class="red-square"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FirstComponent',
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    saluta() {
+      console.log('ti saluti il tuo primo componente');
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+$bgColor: red;
+
+
+@mixin ac-square {
+    width: 150px;
+    height: 150px;
+}
+
+.red-square{
+    @include ac-square();
+    background-color: brown;
+}
+
+/*
+.red {
+   background-color: $bgColor;
+    .inner{
+        background-color: yellowgreen;
+    }
+    &.active{
+        background-color: blue;
+    }
+}
+
+.inner{
+    background-color: yellow;
+}
+*/
+// il preprocessore trasforma il scss in css standard:
+.red {
+    background-color: red;
+}
+
+.red .inner{
+    background-color: yellowgreen;
+}
+
+.red.active{
+    background-color: blue;
+}
+
+.inner{
+     background-color: yellow;
+}
+
+</style>
